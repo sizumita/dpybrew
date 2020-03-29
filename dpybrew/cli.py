3402286caf25f79ed66eff9821abfd4013a94d83
和泉田済人 <sumito@izumita.com>
2020-03-29 21:51:37 +0900

    --versionを追加

--- dpybrew/cli.py
+++ dpybrew/cli.py
@@ -1,16 +1,32 @@
 """Console script for dpybrew."""
 import sys
 import click
+from dpybrew import __version__
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(f'dpybrew {__version__}')
+    ctx.exit()
 
 
 @click.command()
+@click.option('--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True)
 def main(args=None):
     """Console script for dpybrew."""
-    click.echo("Replace this message by putting your code into "
-               "dpybrew.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    click.echo(f'dpybrew {__version__}')
     return 0
 
 
+@click.command(name='list')
+@click.argument('filepath', nargs=1, default='.')
+def extension_list(filepath):
+    """Show list of extensions."""
+    print(sys.path)
+
+
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())
+
