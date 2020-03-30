77f500c4c8cba4205532b4d197a26303ecb14045
和泉田済人 <sumito@izumita.com>
2020-03-30 12:29:39 +0900

    [Add] installコマンド作成

--- dpybrew/cli.py
+++ dpybrew/cli.py
@@ -1,10 +1,18 @@
 """Console script for dpybrew."""
 import sys
-import os
+import urllib.request
 import click
 import pathlib
 from dpybrew import __version__
 from .extensions import FileExtension, PyExtension
+import yaml
+from git import Git
+GIST_URL = "https://gist.githubusercontent.com/sizumita/" \
+           "19ec79e3ad0ecfae89cca665ddf717e1/raw/b04f8b794ca61cc9701ac8edee37edc52b9f5130/modules.yml"
+
+
+def error(message):
+    click.echo(click.style(message, fg='red'))
 
 
 def print_version(ctx, param, value):
@@ -45,6 +53,28 @@ def extension_list(filepath):
             click.echo(click.style(text, fg='yellow', bold=True))
         else:
             click.echo(click.style(str(extension), fg='blue'))
+
+
+@main.command(name='install')
+@click.argument('name', nargs=1)
+def install_extension(name):
+    click.echo(f'Collectiong {name}')
+    target = ""
+    if name.startswith("git+"):
+        target = name[4:]
+    else:
+        with urllib.request.urlopen(GIST_URL) as response:
+            html = response.read()
+        data = yaml.safe_load(html)
+        if name in data.keys():
+            target = data[name]
+
+    if target == "":
+        error(f'  Could not find module "{name}"')
+        return
+
+    Git().clone(target)
+    click.echo(f'Successfully installed {name}')
 
 
 if __name__ == "__main__":
