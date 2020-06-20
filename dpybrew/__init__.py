e9a4d37b724b91925c9397ee7940fdf5d43bfdf4
Sumito Izumita <sumito@izumita.com>
2020-06-20 18:08:19 +0900

    Bump version: 0.2.2 → 0.3.0

--- dpybrew/__init__.py
+++ dpybrew/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """sizumita"""
 __email__ = 'None'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sizumita/dpybrew',
-    version='0.2.2',
+    version='0.3.0',
     zip_safe=False,
 )
